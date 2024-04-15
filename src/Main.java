46fb7e19d3b8cfa4dbddd27d325aabcfc0e76724
Dinis Yakhin <batya.petrovic@gmail.com>
2024-04-15 13:12:00 +0500

    построчное чтение файла, добавление исключения

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int count=0;
+        int countstr=0;
+        int max=0;
+        int min=1024;//максимальная длина строки
         while(true) {
             System.out.println("Введите путь к файлу и нажмите <Enter>: ");
             String path = new Scanner(System.in).nextLine();
@@ -19,6 +24,26 @@ public class Main {
                 System.out.println("Путь указан верно"+" "+"Это файл номер "+count);
 
             }
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if(max<length){max=length;}
+                    if(max>=1024)throw new RuntimeException("строка длиннее 1024 символов");
+                    if(min>length) {min=length;}
+                    countstr++;//счетчик
+                     }
+                System.out.println("общее количество строк в файле: "+countstr);
+                System.out.println("длина самой длинной строки в файле: "+max);
+                System.out.println("длина самой короткой строки в файле: "+min);
+            }
+            catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
         }
     }
 }
