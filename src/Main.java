944021fba36623ff25dd3c4f51949e54dfd6b4a3
Dinis Yakhin <batya.petrovic@gmail.com>
2024-04-16 15:53:58 +0500

    2 задание, сравнение

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +10,7 @@ public class Main {
         int countstr=0;
         int max=0;
         int min=1024;//максимальная длина строки
+        double yndx = 0, gogle = 0;
         while(true) {
             System.out.println("Введите путь к файлу и нажмите <Enter>: ");
             String path = new Scanner(System.in).nextLine();
@@ -31,14 +33,29 @@ public class Main {
 
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
+                    //максимум_минимум символов в строке
                     if(max<length){max=length;}
                     if(max>=1024)throw new RuntimeException("строка длиннее 1024 символов");
                     if(min>length) {min=length;}
-                    countstr++;//счетчик
+                    //максимум_минимум символов в строке
+                    String[] parts = line.split(";");
+                    if (parts.length >= 2) {
+                        String fragment = parts[1];
+                        fragment=fragment.replaceAll("\\s+","");//убираю пробелы
+                        String[] parts2=fragment.split("/");
+                        if (parts2.length >= 2) {
+                            String fragment_res = parts2[0];//все до слэша
+                            if(Objects.equals(fragment_res, "YandexBot")){yndx++;}
+                            if(Objects.equals(fragment_res, "Googlebot")){gogle++;}
+                                         }
+                    }
+
+                    countstr++;//счетчик строк в файле
                      }
                 System.out.println("общее количество строк в файле: "+countstr);
-                System.out.println("длина самой длинной строки в файле: "+max);
-                System.out.println("длина самой короткой строки в файле: "+min);
+                System.out.println("доля яндекса: "+(yndx/countstr));
+                System.out.println("доля гугла: "+(gogle/countstr));
+
             }
             catch (Exception ex) {
                 System.out.println(ex.getMessage());
